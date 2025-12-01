27cdabc6efdc8ba62cc030f75b3b81d74a6fefd0
0hscar <oscar.weber00@gmail.com>
2025-12-01 14:09:21 +0200

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/worldclock-ghpages-react/', // Change this to your repo name for GitHub Pages
+  base: "/worldclocks/", // Change this to your repo name for GitHub Pages
 });
